8bcb64f326b88f94f6abbfe58b19ebe918231934
Oleg Kolpakov <00legas00@gmail.com>
2021-04-11 10:19:20 +0300

    Работаюс классом Router

--- application/core/Router.php
+++ application/core/Router.php
@@ -7,18 +7,38 @@ class Router {
     protected $params = [];
 
     public function __construct(){
-        //
+        $arr = require 'application/config/routes.php';
+        foreach ($arr as $key => $value){
+            $this->add($key, $value);
+        }
     }
 
-    public function add(){
-        //
+    public function add($route, $params){
+        $route = '#^'.$route.'$#';
+        $this->routes[$route] = $params;
     }
 
     public function match(){
-        //
+        $url = trim($_SERVER['REQUEST_URI'], '/');
+        foreach ($this->routes as $route => $params){
+            if (preg_match($route, $url, $matches)){
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
     }
 
     public function run(){
-        echo 'start';
+        if($this->match()){
+            $controller = 'application\controllers\\'.ucfirst($this->params['controller']).'Controller.php';
+            if (class_exists($controller)){
+                echo 'OK';
+            } else {
+                echo 'Не найден: ' . $controller;
+            }
+        } else {
+            echo 'Маршрут не найден';
+        }
     }
 }
